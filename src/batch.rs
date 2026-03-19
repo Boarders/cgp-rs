9f8430ef96d97f288910ac057f74371ab37adce2
Boarders <callan.mcgill@gmail.com>
2026-03-19 12:17:30 -0400

    Changes for generate inputs
    - Use usize over u64 as a small bitvector
    - Add a prop test for truth table column generation

--- src/batch.rs
+++ src/batch.rs
@@ -6,14 +6,14 @@ pub trait BatchEval: Sized {
 }
 
 
-impl BatchEval for u64 {
-    fn generate_inputs(arity : usize) -> Vec<u64> {
+impl BatchEval for usize {
+    fn generate_inputs(arity : usize) -> Vec<usize> {
 	let mut inputs = Vec::new();
 	if arity == 0 {
 	    return inputs
 	}
 	for column in 0..arity {
-	    let mut curr : u64 = 0;
+	    let mut curr : usize = 0;
 	    for i in 0..(1 << arity) {
 		if (i >> column) & 1 == 1 {
 		    curr = curr | (1 << i);
@@ -54,4 +54,37 @@ impl BatchEval for BitVec {
     fn measure_fitness(output: &[Self], target: &[Self]) -> usize {
 	output.iter().zip(target.iter()).map(|(o, t)| (o.clone() ^ t.clone()).count_ones()).sum()
     }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use proptest::prelude::*;
+
+    proptest! {
+	#[test]
+	// Note: for u64 we can only handle up to arity 6 which corresponds to
+	// a bitvector of size 2^6 = 64
+	fn generate_inputs_usize(arity in 1usize..6) {
+	let inputs = usize::generate_inputs(arity);
+	for i in 0..(1 << arity) {
+	    // The generated inputs are the columns of a complete truth table
+	    // the corresponding rows would just be the binary representation
+	    // of the numbers from 0 to 2^arity - 1
+
+	    // It should be the case that if we get the ith bit from each column
+	    // and then bitshift it appropriately, we get back the number of the column
+
+	    let row: usize = inputs.iter().enumerate() // label each column by its index
+		.map(|(k, col)| ((col >> i) & 1usize) << k)
+	    // extract the ith row bit from the kth column and then shift to its placeholder
+		.sum();
+	    // sum up all the bits (same as bitwise or)
+            prop_assert_eq!(row, i);
+	}
+
+	}
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use cartesian_genetic_programming::batch::BatchEval;
 
 fn main() {
-    let inputs : Vec<u64> = BatchEval::generate_inputs(3);
+    let inputs : Vec<usize> = BatchEval::generate_inputs(3);
     for input in inputs {
 	println!("{:#b}", input);
     }
