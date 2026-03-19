9511a0b8b93c9e06c27e9ba14f629d7e05d2840b
Boarders <callan.mcgill@gmail.com>
2026-03-19 01:04:13 -0400

    Add batched instance for BitVec

--- src/batch.rs
+++ src/batch.rs
@@ -1,6 +1,8 @@
+use bitvec::prelude::{BitVec};
+
 pub trait BatchEval: Sized {
     fn generate_inputs(arity: usize) -> Vec<Self>;
-    fn measure_fitness(output: &[Self], target: &[Self]) -> u32;
+    fn measure_fitness(output: &[Self], target: &[Self]) -> usize;
 }
 
 
@@ -22,7 +24,34 @@ impl BatchEval for u64 {
 	inputs
     }
 
-    fn measure_fitness(output: &[Self], target: &[Self]) -> u32 {
-	output.iter().zip(target.iter()).map(|(o, t)| (o ^ t).count_ones()).sum()
+    fn measure_fitness(output: &[Self], target: &[Self]) -> usize {
+	output.iter().zip(target.iter()).map(|(o, t)| (o ^ t).count_ones()).sum::<u32>() as usize
+    }
+}
+
+
+impl BatchEval for BitVec {
+    fn generate_inputs(arity : usize) -> Vec<BitVec> {
+	let mut inputs = Vec::new();
+	if arity == 0 {
+	    return inputs
+	}
+	for column in 0..arity {
+	    let mut curr : BitVec = BitVec::new();
+	    for i in 0..(1 << arity) {
+		if (i >> column) & 1 == 1 {
+		    curr.push(true);
+		}
+		else {
+		    curr.push(false)
+		}
+	    }
+	    inputs.push(curr);
+	}
+	inputs
+    }
+
+    fn measure_fitness(output: &[Self], target: &[Self]) -> usize {
+	output.iter().zip(target.iter()).map(|(o, t)| (o.clone() ^ t.clone()).count_ones()).sum()
     }
 }
