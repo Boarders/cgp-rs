380902841f07103ad6c90c8242df986a31051a1c
Boarders <callan.mcgill@gmail.com>
2026-03-19 00:45:04 -0400

    Add comments to code and fix some type choices

--- src/genome.rs
+++ src/genome.rs
@@ -5,7 +5,7 @@ use rand_xoshiro::Xoshiro256PlusPlus;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct NodeIdx {
-    idx: u64,
+    idx: usize,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -15,11 +15,48 @@ pub struct Node<b> {
     func_idx: b,
 }
 
-pub struct Genome<a> {
-    nodes: Vec<Node<a>>,
+// The nodes field corresponds to a DAG that computes a given generalized circuit
+// which we can think of as follows:
+//
+//
+// A ─────┬───────────────┐
+// │      │               │
+// │      └──► N0 ◄───────┘
+// │           │
+// │  B ──┬────┼──────────┐
+// │      │    ▼   ┌──────┘
+// │      │    N1 ◄┘
+// │      │     └────────────┐
+// │      ├───────────────┐  │
+// │      │               │  │
+// │      └──► N2 ◄───────┘  │
+// │           │             │
+// └───────────┼──────────┐  │
+//             ▼   ┌──────┘  │
+//             N3 ◄┘         │
+//            │ ┌────────────┘
+//            │ │
+//            ▼ ▼
+//            N4 [OUT]
+//
+// This is a NAND network computing XOR where each node has only NAND as the function index:
+//   N0 = NAND(A, A) = !A
+//   N1 = NAND(N0, B) = !(!AB)
+//   N2 = NAND(B, B) = !B
+//   N3 = NAND(N2, A) = !(A!B)
+//   N4 = NAND(N1, N3) = !(!(!AB) AND !(A!B))
+//                     = !!(!AB) + !!(A!B)
+//                     = !AB + A!B
+//                     = A XOR B
+
+pub struct Genome<b> {
+    nodes: Vec<Node<b>>,
     output_nodes: Vec<NodeIdx>
 }
 
+// Here inputs is a slice which we can think of as an environment
+// mapping variables which we identify as indices to values of our
+// input type for the kind of circuit we are computing
 pub fn run_genome<T, b>(genome: &Genome<b>, inputs: &[T]) -> Vec<T>
 where
     b: FunctionSet<T>,
